fb2d75f90b0ce13afe978ad1c35a3bdbdcdfc9f7
Jonas Nikula <jonas.nikula@student.tut.fi>
2017-09-26 22:04:42 +0900

    Add directional derivative images

--- A4/mdaisy-v1.0/nikulaj_image_generation.m
+++ A4/mdaisy-v1.0/nikulaj_image_generation.m
@@ -5,7 +5,7 @@ imageFolder = fullfile('..', 'images');
 %%
 image1 = imread(fullfile(imageFolder, 'face.jpg'));
 image1 = rgb2gray(image1);
-figure; imshow(image1);
+%figure; imshow(image1);
 
 %%
 compute_daisy(image1);
@@ -13,7 +13,7 @@ compute_daisy(image1);
 %%
 image2 = imread(fullfile(imageFolder, 'grasshopper.jpg'));
 image2 = rgb2gray(image2);
-figure; imshow(image2);
+%figure; imshow(image2);
 
 %%
 compute_daisy(image2);
