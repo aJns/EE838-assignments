641c57643f62db1703c863c2ddc5d0af40ef5555
Jonas Nikula <jonas.nikula@student.tut.fi>
2017-09-27 12:52:01 +0900

    Annotate code

--- A4/mdaisy-v1.0/nikulaj_image_generation.m
+++ A4/mdaisy-v1.0/nikulaj_image_generation.m
@@ -8,7 +8,16 @@ image1 = rgb2gray(image1);
 %figure; imshow(image1);
 
 %%
-compute_daisy(image1);
+R  = 15;
+RQ = 3;
+TQ = 8;
+HQ = 8;
+SI = 1;
+LI = 1;
+NT = 1;
+dzy1 = compute_daisy(image1, R, RQ, TQ, HQ, SI, LI, NT);
+descriptor1 = display_descriptor(dzy1, 200, 200);
+pre_dzy1 = init_daisy(image1, R, RQ, TQ, HQ, SI, LI, NT);
 
 %%
 image2 = imread(fullfile(imageFolder, 'grasshopper.jpg'));
