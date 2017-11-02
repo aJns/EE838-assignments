24ead59cf6f864a4fbdd136d70c4727621410961
Jonas Nikula <jonas.nikula@student.tut.fi>
2017-11-02 12:21:21 +0900

    Approximate the line model

--- A6/RANSAC.m
+++ A6/RANSAC.m
@@ -1,7 +1,7 @@
-%% A: RANSAC for line fitting
+%A: RANSAC for line fitting
 % model: y = ax + b
 
-% generate data
+%% generate data
 % data domain: -50, 50
 % number of points: 200
 % inlier thresholds: 0.5, 1, 2, 5
@@ -48,3 +48,85 @@ for i=1:nb_points
         end
     end
 end
+
+test_data = [x y];
+
+clearvars a b x y
+
+
+%% Estimate model with RANSAC
+
+% variables
+% probability that at least one sample has no outliers: 9.99%
+% s = sample size, 2 because a line can be modeled with 2 points
+no_outlier_prob = 0.999;
+sample_size = 2;
+nb_iterations = log(1-no_outlier_prob)./log(1-inlier_ratios.^sample_size);
+
+% Because we have 4 thresholds, and 4 ratios, I assume we are going to estimate
+% the line 4 times.
+
+a_best = zeros(4, 1);
+b_best = zeros(4, 1);
+
+for i=1:length(nb_iterations)
+    best_count = 0;
+    for j=1:nb_iterations(i)
+        line_index = randi([1 200], 2, 1);
+        x = test_data(line_index, 1);
+        y = test_data(line_index, 2);
+
+        a = (y(2)-y(1))/(x(2)-x(1));
+        b = y(1)-x(1)*( (y(2)-y(1)) / (x(2)-x(1)) );
+
+        threshold = inlier_th(i);
+        current_count = 0;
+
+        clearvars x y
+
+        for k=1:length(test_data)
+            x = test_data(k,1);
+            y = test_data(k,2);
+            d = abs(a*x - y + b)/sqrt(a^2 + 1^2);
+            if d < threshold
+                current_count = current_count + 1;
+            end
+        end
+
+        if current_count > best_count
+            best_count = current_count;
+            a_best(i) = a;
+            b_best(i) = b;
+        end
+    end
+end
+
+
+
+%% plotting the resulting line approximations
+
+x = linspace(-50, 50);
+y = a_best*x + b_best;
+
+close all;
+figure;
+
+plot(test_data(:,1), test_data(:,2), 'o');
+hold on;
+plot(x, y, '--');
+hold off;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
