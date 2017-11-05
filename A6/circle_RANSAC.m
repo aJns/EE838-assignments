94d26b1d12248f32b17d45d4e6a02039b99c46bb
Jonas Nikula <jonas.nikula@student.tut.fi>
2017-11-05 16:10:23 +0900

    Use 16 data sets for circle too

--- A6/circle_RANSAC.m
+++ A6/circle_RANSAC.m
@@ -18,7 +18,7 @@ b = 0;
 r = 15;
 circle_model = [a b r];
 
-test_data = generate_data(nb_points, range, inlier_th, inlier_ratios, circle_model);
+test_data = gen_data_sets(nb_points, range, inlier_th, inlier_ratios, circle_model);
 
 clearvars a b r
 
@@ -30,35 +30,32 @@ clearvars a b r
 % s = sample size, 3 because a circle can be modeled with 3 points
 no_outlier_prob = 0.999;
 sample_size = 3;
-nb_iterations = log(1-no_outlier_prob)./log(1-inlier_ratios.^sample_size);
-
-% Because we have 4 thresholds, and 4 ratios, I assume we are going to estimate
-% the line 4 times.
-
-est_count = length(nb_iterations);
-
-a_best = zeros(est_count, 3);
-b_best = zeros(est_count, 3);
-r_best = zeros(est_count, 3);
 
 mi = 1;
-for i=1:est_count
-    best_count = 0;
-    for j=1:nb_iterations(i)
-        line_index = randi([1 length(test_data)], sample_size, 1);
-        x = test_data(line_index, 1);
-        y = test_data(line_index, 2);
+a_best = zeros(length(inlier_th), length(inlier_ratios), 3);
+b_best = zeros(length(inlier_th), length(inlier_ratios), 3);
+r_best = zeros(length(inlier_th), length(inlier_ratios), 3);
 
-        model = approx_model(x, y);
+for i=1:length(inlier_th)
+    for j=1:length(inlier_ratios)
+        best_count = 0;
+        nb_iterations = log(1-no_outlier_prob)/log(1-inlier_ratios(j)^sample_size);
+        for k=1:nb_iterations
+            line_index = randi([1 size(test_data, 1)], sample_size, 1);
+            x = test_data(line_index, 1, i, j);
+            y = test_data(line_index, 2, i, j);
 
-        threshold = inlier_th(i);
-        current_count = count_inliers(test_data, model, threshold);
+            model = approx_model(x, y);
 
-        if current_count > best_count
-            best_count = current_count;
-            a_best(i, mi) = model(1);
-            b_best(i, mi) = model(2);
-            r_best(i, mi) = model(3);
+            threshold = inlier_th(i);
+            current_count = count_inliers(test_data(:,:,i,j), model, threshold);
+
+            if current_count > best_count
+                best_count = current_count;
+                a_best(i, j, mi) = model(1);
+                b_best(i, j, mi) = model(2);
+                r_best(i, j, mi) = model(3);
+            end
         end
     end
 end
@@ -67,28 +64,30 @@ end
 %% R-RANSAC
 
 mi = 2;
-for i=1:est_count
-    best_count = 0;
-    threshold = inlier_th(i);
+for i=1:length(inlier_th)
+    for j=1:length(inlier_ratios)
+        best_count = 0;
+        nb_iterations = log(1-no_outlier_prob)/log(1-inlier_ratios(j)^sample_size);
+        for k=1:nb_iterations
+            line_index = randi([1 size(test_data, 1)], sample_size, 1);
+            x = test_data(line_index, 1, i, j);
+            y = test_data(line_index, 2, i, j);
 
-    for j=1:nb_iterations(i)
-        circle_index = randi([1 length(test_data)], sample_size, 1);
-        x = test_data(circle_index, 1);
-        y = test_data(circle_index, 2);
+            model = approx_model(x, y);
+            threshold = inlier_th(i);
 
-        model = approx_model(x, y);
-        
-        if ~passed_preval(test_data, nb_iterations(i), model, threshold)
-            continue
-        end
+            if ~passed_preval(test_data(:,:,i,j), nb_iterations, model, threshold)
+                continue
+            end
 
-        current_count = count_inliers(test_data, model, threshold);
+            current_count = count_inliers(test_data(:,:,i,j), model, threshold);
 
-        if current_count > best_count
-            best_count = current_count;
-            a_best(i, mi) = model(1);
-            b_best(i, mi) = model(2);
-            r_best(i, mi) = model(3);
+            if current_count > best_count
+                best_count = current_count;
+                a_best(i, j, mi) = model(1);
+                b_best(i, j, mi) = model(2);
+                r_best(i, j, mi) = model(3);
+            end
         end
     end
 end
@@ -98,23 +97,26 @@ end
 
 mi=3;
 
-for i=1:length(nb_iterations)
-    best_score = 0;
-    for j=1:nb_iterations(i)
-        line_index = randi([1 length(test_data)], sample_size, 1);
-        x = test_data(line_index, 1);
-        y = test_data(line_index, 2);
+for i=1:length(inlier_th)
+    for j=1:length(inlier_ratios)
+        best_count = 0;
+        nb_iterations = log(1-no_outlier_prob)/log(1-inlier_ratios(j)^sample_size);
+        for k=1:nb_iterations
+            line_index = randi([1 size(test_data, 1)], sample_size, 1);
+            x = test_data(line_index, 1, i, j);
+            y = test_data(line_index, 2, i, j);
 
-        model = approx_model(x, y);
+            model = approx_model(x, y);
 
-        threshold = inlier_th(i);
-        current_score = calc_msac_score(test_data, model, threshold);
+            threshold = inlier_th(i);
+            current_score = calc_msac_score(test_data(:,:,i,j), model, threshold);
 
-        if current_score > best_score
-            best_score = current_score;
-            a_best(i, mi) = model(1);
-            b_best(i, mi) = model(2);
-            r_best(i, mi) = model(3);
+            if current_count > best_count
+                best_count = current_count;
+                a_best(i, j, mi) = model(1);
+                b_best(i, j, mi) = model(2);
+                r_best(i, j, mi) = model(3);
+            end
         end
     end
 end
@@ -123,21 +125,54 @@ end
 %% plotting the resulting line approximations
 close all;
 
+nb_th = length(inlier_th);
+nb_rt = length(inlier_ratios);
+
 for mi=1:3
-    ang=linspace(0, 2*pi, nb_points);
-    x=(r_best(:, mi)*cos(ang) + a_best(:, mi))';
-    y=(r_best(:, mi)*sin(ang) + b_best(:, mi))';
-
     figure;
+    counter = 1;
+    for i=1:nb_th
+        for j=1:nb_rt
 
-    plot(test_data(:,1), test_data(:,2), 'o');
-    hold on;
-    plot(x, y);
-    hold off;
-    legend('test data', num2str(inlier_th(1)), num2str(inlier_th(2)), num2str(inlier_th(3)),num2str(inlier_th(4)));
+    ang=linspace(0, 2*pi, nb_points);
+    x=(r_best(i,j,mi)*cos(ang) + a_best(i,j,mi))';
+    y=(r_best(i,j,mi)*sin(ang) + b_best(i,j,mi))';
+
+            subplot(nb_th, nb_rt, counter)
+            plot(test_data(:,1,i,j), test_data(:,2,i,j), 'o');
+            hold on;
+            plot(x, y);
+            hold off;
+            title([ 'threshold:' num2str(inlier_th(i)) ' ratio:' num2str(inlier_ratios(j)) ]);
+            counter = counter + 1;
+        end
+    end
 end
 
 
+
+%% Save the mse results to matrix and matrix to csv
+% each row has the same threshold
+% each column the same ratio of inliers
+
+ground_truth = circle_model;
+
+mse_results = zeros(nb_th, nb_rt, 3);
+
+for mi=1:3
+    for i=1:nb_th
+        for j=1:nb_rt
+            approx_model = [a_best(i, j, mi) b_best(i, j, mi) r_best(i, j, mi)];
+            mse_results(i, j, mi) = compare_ground_truth(ground_truth, approx_model);
+        end
+    end
+end
+
+csvwrite('circle_mse_RANSAC.csv', mse_results(:,:,mi));
+
+csvwrite('circle_mse_R-RANSAC.csv', mse_results(:,:,mi));
+
+csvwrite('circle_mse_MSAC.csv', mse_results(:,:,mi));
 
 
 
--- A6/compare_ground_truth.m
+++ A6/compare_ground_truth.m
@@ -14,6 +14,25 @@ if length(ground_truth) == 2
     mean_squared_error = immse(y_truth, y_approx);
 
 else
+    a_truth = ground_truth(1);
+    b_truth = ground_truth(2);
+    r_truth = ground_truth(3);
+
+    a_approx = approx_model(1);
+    b_approx = approx_model(2);
+    r_approx = approx_model(3);
+
+    ang=linspace(0, 2*pi);
+    x_truth = (r_truth*cos(ang) + a_truth)';
+    y_truth = (r_truth*sin(ang) + b_truth)';
+    x_approx = (r_approx*cos(ang) + a_approx)';
+    y_approx = (r_approx*sin(ang) + b_approx)';
+
+    truth = [x_truth y_truth];
+    approx = [x_approx y_approx];
+
+    mean_squared_error = immse(truth, approx);
+
 end
 
 
