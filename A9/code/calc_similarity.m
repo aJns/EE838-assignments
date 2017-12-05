d0978647f5ff06ea055bb2faf4db1590baa21269
Jonas Nikula <jonas.nikula@student.tut.fi>
2017-12-05 22:59:39 +0900

    Prune code

--- A9/code/calc_similarity.m
+++
@@ -1,7 +0,0 @@
-function similarity = calc_similarity(feature1, feature2) 
-
-diff = feature1 - feature2;
-ssd = sum(diff(:).^2);
-
-similarity = ssd;
-
--- A9/code/normalize_for_DLT.m
+++
@@ -1,5 +0,0 @@
-function [points1_hat, T1, points2_hat, T2] = normalize_for_DLT(points1, points2)
-
-[points1_hat, T1] = normalize_points(points1);
-[points2_hat, T2] = normalize_points(points2);
-
